9d43dbc0786d7a4d5f360543e27bbb7e0ae948a3
MKGilby <gilby@mksztsz.hu>
2025-04-03 10:49:37 +0200

    Fix in build scripts
    - Added removing temporary directory.

--- source/BuildRelease_x64.bat
+++ source/BuildRelease_x64.bat
@@ -2,4 +2,5 @@
 if not exist lib\ (mkdir lib) else (del /Q lib\*)
 echo Compiling x64 release...
 C:\lazarus\fpc\3.2.2\bin\x86_64-win64\fpc.exe -Twin64 -Px86_64 -l- -MDelphi -Scghi -CX -O3 -XX -v0 -vm6058,5024 -Filib -Fuunits -Fuunits\SDL2 -Fu. -FUlib -FE..\release\x64 -oLadybugs.exe Ladybugs.lpr
+RMDIR "lib" /S /Q
 
--- source/BuildRelease_x86.bat
+++ source/BuildRelease_x86.bat
@@ -2,4 +2,5 @@
 if not exist lib\ (mkdir lib) else (del /Q lib\*)
 echo Compiling x86 release...
 C:\lazarus\fpc\3.2.2\bin\x86_64-win64\fpc.exe -l- -Twin32 -Pi386 -MDelphi -Scghi -CX -O3 -XX -v0 -vm6058,5024 -Filib -Fuunits -Fuunits\SDL2 -Fu. -FUlib\ -FE..\release\x86 -oLadybugs.exe Ladybugs.lpr
+RMDIR "lib" /S /Q
 
