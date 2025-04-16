c34ea54005ff1d22dd29f0f7f506b63e0cb82233
MKGilby <gilby@mksztsz.hu>
2025-04-16 09:09:26 +0200

    Update scripts
    - Set FPCDIR in setenv to point to your FreePascal installation dir. Other scripts will use this variable to access fpc.exe

--- tools/source/BuildTools.bat
+++ tools/source/BuildTools.bat
@@ -1,16 +1,17 @@
 @echo off
+call ..\..\work\setenv.bat
 if not exist lib\ (mkdir lib) else (del /Q lib\*)
 echo Compiling MKConv2...
-C:\lazarus\fpc\3.2.2\bin\x86_64-win64\fpc.exe -l- -MDelphi -Scghi -CX -O3 -XX -v0 -vm6058,5024 -Filib -Fu..\..\source\units -Fu..\..\source\units\* -Fu. -FUlib -FE..\ -omkconv2.exe mkconv2.lpr
+%FPCDIR%\fpc.exe -l- -MDelphi -Scghi -CX -O3 -XX -v0 -vm6058,5024 -Filib -Fu..\..\source\units -Fu..\..\source\units\* -Fu. -FUlib -FE..\ -omkconv2.exe mkconv2.lpr
 rem C:\lazarus\fpc\3.2.2\bin\x86_64-win64\fpc.exe -MDelphi -Scghi -CX -O3 -XX -l -vewi -vm6058,5024 -Filib\x86_64-win64 -Fuunits -Fuunits\* -FuC:\lazarus\lcl\units\x86_64-win64\win32 -FuC:\lazarus\lcl\units\x86_64-win64 -FuC:\lazarus\components\freetype\lib\x86_64-win64 -FuC:\lazarus\components\lazutils\lib\x86_64-win64 -FuC:\lazarus\packager\units\x86_64-win64 -Fu. -FUlib\x86_64-win64 -FE..\release\x64 -oBurdockPaint.exe -dLCL -dLCLwin32 BurdockPaint.lpr
 
 echo Compiling MAD4...
-C:\lazarus\fpc\3.2.2\bin\x86_64-win64\fpc.exe -l- -MDelphi -Scghi -CX -O3 -XX -v0 -vm6058,5024 -Filib -Fu..\..\source\units -Fu..\..\source\units\* -Fu. -FUlib -FE..\ -oMAD4.exe MAD4.lpr
+%FPCDIR%\fpc.exe -l- -MDelphi -Scghi -CX -O3 -XX -v0 -vm6058,5024 -Filib -Fu..\..\source\units -Fu..\..\source\units\* -Fu. -FUlib -FE..\ -oMAD4.exe MAD4.lpr
 
 echo Compiling BuildBugs...
-C:\lazarus\fpc\3.2.2\bin\x86_64-win64\fpc.exe -l- -MDelphi -Scghi -CX -O3 -XX -v0 -vm6058,5024 -Filib -Fu..\..\source\units -Fu..\..\source\units\* -Fu. -FUlib -FE..\ -oBuildBugs.exe BuildBugs.lpr
+%FPCDIR%\fpc.exe -l- -MDelphi -Scghi -CX -O3 -XX -v0 -vm6058,5024 -Filib -Fu..\..\source\units -Fu..\..\source\units\* -Fu. -FUlib -FE..\ -oBuildBugs.exe BuildBugs.lpr
 
 echo Compiling BuildMushrooms...
-C:\lazarus\fpc\3.2.2\bin\x86_64-win64\fpc.exe -l- -MDelphi -Scghi -CX -O3 -XX -v0 -vm6058,5024 -Filib -Fu..\..\source\units -Fu..\..\source\units\* -Fu. -FUlib -FE..\ -oBuildMushrooms.exe BuildMushrooms.lpr
+%FPCDIR%\fpc.exe -l- -MDelphi -Scghi -CX -O3 -XX -v0 -vm6058,5024 -Filib -Fu..\..\source\units -Fu..\..\source\units\* -Fu. -FUlib -FE..\ -oBuildMushrooms.exe BuildMushrooms.lpr
 
 RMDIR "lib" /S /Q
--- work/setenv.bat
+++ work/setenv.bat
@@ -1,3 +1,6 @@
+rem Set this according to your environment
+set FPCDIR="C:\lazarus\fpc\3.2.2\bin\x86_64-win64"
+
+rem Don't change them
 set TOOLSDIR="..\..\tools"
 set DATADIR="..\..\data"
-
