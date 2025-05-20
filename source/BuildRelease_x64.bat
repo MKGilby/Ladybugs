db182dc4b62aa37d26ee03ded20c5563e868bed9
HUNGilby <gilby@mksztsz.hu>
2025-05-20 19:21:58 +0200

    Changed build scripts
    - Added a missing setenv
    - Removed printouts

--- source/BuildRelease_x64.bat
+++ source/BuildRelease_x64.bat
@@ -1,7 +1,6 @@
 @echo off
 call ..\work\setenv.bat
 if not exist lib\ (mkdir lib) else (del /Q lib\*)
-echo Compiling x64 release...
 %FPCDIR%\fpc.exe -Twin64 -Px86_64 -l- -MDelphi -Scghi -CX -O3 -XX -v0 -vm6058,5024 -Filib -Fuunits -Fuunits\SDL2 -Fu. -FUlib -FE..\release\x64 -oLadybugs.exe Ladybugs.lpr
 RMDIR "lib" /S /Q
 
--- source/BuildRelease_x86.bat
+++ source/BuildRelease_x86.bat
@@ -1,6 +1,6 @@
 @echo off
 if not exist lib\ (mkdir lib) else (del /Q lib\*)
-echo Compiling x86 release...
+call ..\work\setenv.bat
 %FPCDIR%\fpc.exe -l- -Twin32 -Pi386 -MDelphi -Scghi -CX -O3 -XX -v0 -vm6058,5024 -Filib -Fuunits -Fuunits\SDL2 -Fu. -FUlib\ -FE..\release\x86 -oLadybugs.exe Ladybugs.lpr
 RMDIR "lib" /S /Q
 
